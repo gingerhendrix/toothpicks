1e0d571fd5ab37f7f9d8bc9d3fad5085c3484e04
Gareth Andrew <gingerhendrix@gmail.com>
2019-07-26 22:25:53 +0100

    Updates

---
+++ precache-manifest.2d4013f58be8f2d0fbbbe00a76f65781.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2a7907bf65891c7ae84929d73fda7e4",
+    "url": "/toothpicks/index.html"
+  },
+  {
+    "revision": "6365570918b4a5b6f5e4",
+    "url": "/toothpicks/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e2c3638560d2b0c674d6",
+    "url": "/toothpicks/static/js/2.5d9e8d3e.chunk.js"
+  },
+  {
+    "revision": "6365570918b4a5b6f5e4",
+    "url": "/toothpicks/static/js/main.79a33466.chunk.js"
+  },
+  {
+    "revision": "08f7169a69cea4f52814",
+    "url": "/toothpicks/static/js/runtime~main.955d9fdd.js"
+  }
+]);
