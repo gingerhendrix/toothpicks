32204b1d84de33ac4cfb3720e64b921af609f78d
Gareth Andrew <gingerhendrix@gmail.com>
2019-07-26 22:14:01 +0100

    Cosmetic improvements

--- src/Pattern/Component.tsx
+++ src/Pattern/Component.tsx
@@ -1,21 +1,38 @@
-import React, {useMemo} from 'react';
+import React from 'react';
 import {Generation as ToothpickGeneration, ToothpickPattern, Point} from '../lib/pattern';
 
-const Line: React.FC<{ends: [Point, Point]}> = ({ends: [start, finish]}) =>
+const Line: React.FC<{ends: [Point, Point], current: boolean}> = ({ends: [start, finish], current}) =>
   <line x1={start.x} y1={start.y} x2={finish.x} y2={finish.y} style={{
-    stroke: 'rgb(0,0,255)',
-    strokeWidth: 0.05,
+    stroke: current ? 'rgb(0,0,255)' : 'rgb(0,255,0)',
+    transition: 'stroke 4s',
+    strokeWidth:  0.5,
   }} />
 
-const Generation: React.FC<{generation: ToothpickGeneration}> = ({generation}) => (
+const GenerationBase: React.FC<{generation: ToothpickGeneration, current: boolean}> = ({generation, current=false}) => (
   <g>
     {
       generation.toothpicks.map(toothpick =>
-        <Line ends={toothpick.ends()} key={toothpick.center.toString()} />
+        <Line ends={toothpick.ends()} key={toothpick.center.toString()} current={current} />
       )
     }
   </g>
 );
+
+const Generation = React.memo(GenerationBase);
+
+export const BoundingBox: React.FC<{gen: number}> = ({gen}) => {
+  const x = -(gen/2);
+  const y = -(gen/2);
+  const width = gen;
+  const height = gen;
+  const style = {
+    stroke: "red",
+    strokeWidth: 0.5,
+    fill: "none",
+  }
+
+  return <rect x={x} y={y} width={width} height={height} style={style} />
+};
 
 interface ComponentProps {
   pattern: ToothpickPattern,
@@ -24,11 +41,11 @@ interface ComponentProps {
 }
 
 const Component: React.FC<ComponentProps> = ({pattern, size, scale}) => (
-  <svg width={size} height={size}>
+  <svg width={size} height={size} style={{backgroundColor: 'black'}}>
     <g transform={`translate(${size/2}, ${size/2}) scale(${scale})`} >
     {
       pattern.generations.map((generation, idx) =>
-        <Generation generation={generation} key={idx} />
+        <Generation generation={generation} key={idx} current={idx === pattern.generations.length - 1} />
       )
     }
     </g>
--- src/Pattern/Container.tsx
+++ src/Pattern/Container.tsx
@@ -1,18 +1,40 @@
-import React, {useMemo} from 'react';
+import React, {useMemo, useState, useRef, useEffect} from 'react';
 import {ToothpickPattern} from '../lib/pattern';
 import Component from './Component';
+
+const useTick = (duration: number, max: number) => {
+  const [frame, setFrame] = useState(1);
+  const frameRef = useRef(frame);
+  useEffect(() => {
+    const update = () => {
+      frameRef.current = frameRef.current + 1;
+      setFrame(frameRef.current);
+      if(frameRef.current < max) {
+        window.setTimeout(update, duration);
+      }
+    }
+    const interval = window.setTimeout(update, duration);
+    return () => window.clearTimeout(interval);
+  }, [duration, max]);
+
+  return frame;
+}
 
 const Container: React.FC = () => {
   const pattern = useMemo(() => {
     const pattern = new ToothpickPattern();
     (window as any).pattern = pattern;
-    for(let i = 0; i < 210; i++) {
-      pattern.buildNextGeneration();
-    }
     return pattern;
   }, []);
 
-  return <Component pattern={pattern} size={1200} scale={4} />;
+  const tick = useTick(100, 1000)
+  if(tick > pattern.generations.length) {
+    pattern.buildNextGeneration();
+  }
+  const size = 800;
+  const scale = (0.9*size)/pattern.generations.length;
+
+  return <Component pattern={pattern} size={size} scale={scale} />;
 }
 
 export default Container;
